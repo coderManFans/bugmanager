2bb3b3e6ca4b1bf2f045c1ee9eb4330d4c26bd2b
fanchunshuai <2217011466@qq.com>
2015-03-19 16:11:22 +0800

    add pom.xml

---
+++ bugmanagerbasic/src/main/java/cn/com/bugmanger/ajax/response/ReturnState.java
@@ -0,0 +1,12 @@
+package cn.com.bugmanger.ajax.response;
+
+/**
+ * Ajax返回状态
+ *
+ * @author gaoxinyu
+ */
+public enum ReturnState {
+    OK,
+    WARNING,
+    ERROR
+}
---
+++ bugmanagerbasic/src/main/java/cn/com/bugmanger/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by fcs on 2015/3/19.
+ * 该包下为 bugmanager 项目的基本模块
+ */
+package cn.com.bugmanger;
