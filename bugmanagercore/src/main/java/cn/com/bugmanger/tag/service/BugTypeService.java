8caeac2b30ac7466de0b1784d53f233cee91dcfd
fanchunshuai <2217011466@qq.com>
2015-03-19 22:20:41 +0800

    添加tag模块代码 2015年3月19日22:20:17

--- bugmanagercore/src/main/java/cn/com/bugmanger/tag/service/BugTypeService.java
+++ bugmanagercore/src/main/java/cn/com/bugmanger/tag/service/BugTypeService.java
@@ -1,7 +1,43 @@
 package cn.com.bugmanger.tag.service;
 
+import cn.com.bugmanger.tag.dao.BugTypeDao;
+import cn.com.bugmanger.tag.model.BugType;
+import cn.com.bugmanger.tag.model.TagType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by fcs on 2015/3/19.
  */
+@Service("bugTypeService")
 public class BugTypeService {
+    @Autowired
+    private BugTypeDao bugTypeDao;
+    private  int addBugType(BugType  bugType){
+        int affectNum = bugTypeDao.addBugType(bugType);
+        return affectNum;
+    }
+
+    private int updateBugType(BugType bugType){
+        int affectNum = bugTypeDao.updateBugType(bugType);
+        return 0;
+    }
+
+    private void deleteBugType(final int bugTypeid){
+        bugTypeDao.deleteBugTypeById(bugTypeid);
+    }
+
+    private List<BugType> getBugTypeList(){
+        return bugTypeDao.getBugTypeList();
+    }
+
+    private List<TagType> getTagTypeListByBugid(int bugTypeId){
+        return bugTypeDao.getTagTypeListByTagId(bugTypeId);
+    }
+
+
+
+
 }
--- bugmanagercore/src/main/java/cn/com/bugmanger/tag/service/TagTypeService.java
+++ bugmanagercore/src/main/java/cn/com/bugmanger/tag/service/TagTypeService.java
@@ -1,7 +1,51 @@
 package cn.com.bugmanger.tag.service;
 
+import cn.com.bugmanger.tag.dao.TagTypeDao;
+import cn.com.bugmanger.tag.model.BugType;
+import cn.com.bugmanger.tag.model.TagType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by fcs on 2015/3/19.
  */
+@Service("tagTypeService")
 public class TagTypeService {
+    @Autowired
+    private TagTypeDao tagTypeDao;
+
+    /**
+     * @param tagTypeId
+     * @return
+     */
+    private  int getTagTypeByTagTypeId(int tagTypeId){
+        int affectNum = tagTypeDao.getTagTypeById(tagTypeId);
+        return  affectNum;
+    }
+
+    /**
+     * @param tagTypeId
+     */
+    private void deleteTagTypeById(int tagTypeId){
+        tagTypeDao.deleteTagTypeByTagId(tagTypeId);
+    }
+
+    /**
+     * @param tagType
+     * @return
+     */
+    private int updateTagType(TagType tagType){
+        return 0;
+    }
+
+    private List<TagType> getTagTypeList(){
+        return tagTypeDao.getTagTypeList();
+    }
+
+    private List<BugType>  getBugTypeListByTagId(int tagId){
+        return tagTypeDao.getBugTypeListByTagId(tagId);
+    }
+
 }
